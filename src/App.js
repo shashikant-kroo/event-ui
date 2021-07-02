2731292e08a55fbc94d46f26db10eac80d43e89b
shashikant-kroo <shashikant.dwivedi@rkroo.com>
2021-07-02 12:33:57 +0530

    [Hackathon:event->source]: showing events for PAYMENT_ACCOUNT resource Authors: @shashikant-kroo

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import {fetchMicroserviceData} from "./redux/microservice/microservice.action"
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.data =  null
+    this.data = null
     this.state = {
-      data :  [
+      data: [
         ['Name', 'Manager', 'ToolTip']
       ]
     }
@@ -33,16 +33,23 @@ class App extends React.Component {
   }
 
 
-  showAccountResources (){
-    const {accountService} = this.props.serviceData?.resourcesByService
-    const resource = accountService
-      .map(serviceName => this.createEntry(serviceName, microServiceType.ACCOUNT_MICRO_SERVICE))
+  showResources(service, name) {
+    const resources = service
+      .map(serviceName => this.createEntry(serviceName, name))
 
-    this.setState({data: [...this.state.data, ...resource]})
+    this.setState({data: [...this.state.data, ...resources]})
   }
 
+  showEvents = (resource, resourceName) => {
+    const events = resource
+      .map(eventName => this.createEntry(eventName, resourceName))
+
+    this.setState({data: [...this.state.data, ...events]})
+  }
+
+
   updateInitialStateForMicroservice = () => {
-    if(this.props.serviceData?.services) {
+    if (this.props.serviceData?.services) {
       const microservices = Object.keys(this.props.serviceData?.services)
       microservices.forEach(service => {
         this.state.data.push(
@@ -59,9 +66,11 @@ class App extends React.Component {
     }
   }
 
-  handleClickEvent =  ({chartWrapper}) => {
+  handleClickEvent = ({chartWrapper}) => {
     const chart = chartWrapper.getChart()
     const selection = chart.getSelection()
+    const {resourcesByService} = this.props.serviceData
+    const {eventsByResource} = this.props.serviceData
 
     if (selection.length === 1) {
       const [selectedItem] = selection
@@ -71,9 +80,19 @@ class App extends React.Component {
       console.log("selectedItem :", dataTable.getValue(row, 0))
       switch (dataTable.getValue(row, 0)) {
         case microServiceType.ACCOUNT_MICRO_SERVICE:
-          this.showAccountResources()
+          this.showResources(
+            resourcesByService?.accountService,
+            microServiceType.ACCOUNT_MICRO_SERVICE
+          )
+          break;
         case resourceType.accountServiceType.PREPAID_ACCOUNT:
+          break;
         case resourceType.accountServiceType.PAYMENT_ACCOUNT:
+          this.showEvents(
+            eventsByResource?.paymentAccount,
+            resourceType.accountServiceType.PAYMENT_ACCOUNT
+          )
+          break;
         case resourceType.accountServiceType.RANDOM_RESOURCE:
       }
 
@@ -111,10 +130,10 @@ class App extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchMicroservicesData : () => dispatch(fetchMicroserviceData())
+  fetchMicroservicesData: () => dispatch(fetchMicroserviceData())
 })
 
-const mapStateToProps = ({microserviceData : {serviceData}}) => ({
+const mapStateToProps = ({microserviceData: {serviceData}}) => ({
   serviceData
 })
 
